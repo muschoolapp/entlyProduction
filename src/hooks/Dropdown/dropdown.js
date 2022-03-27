6ddd3e5a7050b178465d4f254250dee495e67bfb
tylrdnns <68363565+tylrdnns@users.noreply.github.com>
2022-03-27 11:17:33 -0500

    Add files via upload

---
+++ src/hooks/Dropdown/dropdown.js
@@ -0,0 +1,31 @@
+import React from "react";
+import {
+  DropdownWrapper,
+  StyledSelect,
+  StyledOption,
+  StyledLabel,
+} from "./dropdownstyles.js";
+
+export function Dropdown(props) {
+  return (
+    <DropdownWrapper 
+      action={props.action}
+      onChange={props.onChange}
+    >
+      <StyledLabel htmlFor="services">
+        {props.formLabel}
+      </StyledLabel>
+      <StyledSelect id="services" name="services">
+        {props.children}
+      </StyledSelect>
+    </DropdownWrapper>
+  );
+}
+
+export function Option(props) {
+  return (
+    <StyledOption selected={props.selected}>
+      {props.value}
+    </StyledOption>
+  );
+}
---
+++ src/hooks/Dropdown/dropdownstyles.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const DropdownWrapper = styled.form`
+  display: flex;
+  flex-flow: column;
+  justify-content: flex-start;
+`;
+export const StyledSelect = styled.select`
+  max-width: 100%;
+  height: 100%;
+  padding: 0.5rem;
+  margin-bottom: 1rem;
+`;
+
+export const StyledOption = styled.option`
+  color: ${(props) => (props.selected ? "lightgrey" : "black")};
+`;
+
+export const StyledLabel = styled.label`
+  margin-bottom: 1rem;
+`;
